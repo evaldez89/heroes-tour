9cd8a7f53123e35dccf20372359fd8682b6105ed
Emmanuel Valdez <evaldez89@gmail.com>
2018-10-08 23:20:02 -0400

    get heroes from api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,17 +3,24 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heores';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
+
+  private heroesUrl = 'http://127.0.0.1:8000/heroes/';
+
 
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   logMessage(message: string): void {
@@ -22,6 +29,10 @@ export class HeroService {
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(`${this.heroesUrl}${id}`);
+  }
+
+  private log(message: string) {
+    this.messageService.add(message);
   }
 }
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,5 +1,8 @@
 export class Hero {
   id: number;
   name: string;
-
+  super_power?: string = null;
+  strength?: number = null;
+  inserted_on?: Date = null;
+  updated_on?: Date = null;
 }
